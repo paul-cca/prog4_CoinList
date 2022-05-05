6eb93d871eec51fc3cc83ed7e1fe915a73edba57
Paul Camerloher <paul@camerloher.eu>
2022-05-05 16:01:52 +0200

    [s04/prog4/practice/01] adopt quote class

--- src/main/java/at/ac/fhstp/crs/Token.java
+++ src/main/java/at/ac/fhstp/crs/Token.java
@@ -1,66 +1,63 @@
 package at.ac.fhstp.crs;
 
+import at.ac.fhstp.crs.api.IAPIConnector.TIMESPAN;
+import java.util.Dictionary;
+
 public class Token {
 
-  private String name, symbol;
-  public String getName() {
-    return name;
-  }
+  private final String name, symbol;
 
-
-  public void setName(String name) {
+  public Token(String symbol, String name) {
+    this.symbol = symbol;
     this.name = name;
   }
 
+  public String getName() {
+    return name;
+  }
 
   public String getSymbol() {
     return symbol;
   }
 
-
-  public void setSymbol(String symbol) {
-    this.symbol = symbol;
-  }
-
-
   public Quote getCoinQuote() {
     return coinQuote;
   }
-
 
   public void setCoinQuote(Quote coinQuote) {
     this.coinQuote = coinQuote;
   }
 
-
   public Quote getDollarQuote() {
     return dollarQuote;
   }
-
 
   public void setDollarQuote(Quote dollarQuote) {
     this.dollarQuote = dollarQuote;
   }
 
-
   private Quote coinQuote;
   private Quote dollarQuote;
 
-
   public class Quote {
+
     private float price;
+    private Dictionary<TIMESPAN, Double> percentChange;
+
     public float getPrice() {
       return price;
     }
+
     public void setPrice(float price) {
       this.price = price;
     }
-    public double getPercentChange24h() {
-      return percentChange24h;
+
+    public double getPercentChange(TIMESPAN timespan) {
+      return percentChange.get(timespan);
     }
-    public void setPercentChange24h(double percentChange24h) {
-      this.percentChange24h = percentChange24h;
+
+    public void setPercentChange(TIMESPAN timespan, double percentChange) {
+      this.percentChange.put(timespan, percentChange);
     }
-    private double percentChange24h;
   }
 }
--- src/main/java/at/ac/fhstp/crs/api/CoinMarketAPIConnector.java
+++ src/main/java/at/ac/fhstp/crs/api/CoinMarketAPIConnector.java
@@ -1,29 +1,35 @@
 package at.ac.fhstp.crs.api;
 
-import java.util.List;
-
-import org.json.JSONObject;
-
 import at.ac.fhstp.crs.Token;
+import at.ac.fhstp.crs.TokenBuilder;
+import at.ac.fhstp.crs.Token.Quote;
+
+import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class CoinMarketAPIConnector implements IAPIConnector {
 
-    @Override
-    public List<Token> getPopularTokens() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+  @Override
+  public List<Token> getPopularTokens() {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-    @Override
-    public List<Token> getTopMoversToken(int amount, TIMESPAN timespan) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+  @Override
+  public List<Token> getTopMoversToken(int amount, TIMESPAN timespan) {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
+  private Token convertToToken(JSONObject object) {
+    TokenBuilder builder = new TokenBuilder(
+      object.getString("symbol"),
+      object.getString("name")
+    );
 
-    private Token convertToToken(JSONObject object) {
-       Token t = new Token(); 
-    }
-    
+    Quote coinQuote = new Quote();
+
+    return builder.toToken();
+  }
 }
-
