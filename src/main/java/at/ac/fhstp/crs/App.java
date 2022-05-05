796f1b6a3c1b17b4418afa4a52bc5bb84b31ccc1
Bernhard Steindl <git@e9a.at>
2022-05-05 17:10:13 +0200

    Test code for Samuel's implementation

--- src/main/java/at/ac/fhstp/crs/App.java
+++ src/main/java/at/ac/fhstp/crs/App.java
@@ -1,7 +1,12 @@
 package at.ac.fhstp.crs;
 
+import at.ac.fhstp.crs.api.CoinMarketAPIConnector;
+import at.ac.fhstp.crs.api.IAPIConnector;
 import at.ac.fhstp.crs.menu.ConsoleMenu;
 import at.ac.fhstp.crs.menu.IMenu;
+import com.harium.dotenv.Env;
+
+import java.util.List;
 
 /**
  * Hello world!
@@ -11,6 +16,9 @@ public class App
 {
     public static void main( String[] args )
     {
+        IAPIConnector connector =CoinMarketAPIConnector.getInstance(Env.get("API_KEY"));
+        //List<Token> popToken = connector.getPopularTokens(20);
+
         IMenu menu = new ConsoleMenu();
         menu.displayPopularTokens();
     }
