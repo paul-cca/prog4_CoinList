ba0c956409baf18fbe5f1b2040f4b2936b177217
Bernhard Steindl <git@e9a.at>
2022-05-09 08:06:30 +0200

    rebuild IMenu

--- src/main/java/at/ac/fhstp/crs/menu/IMenu.java
+++ src/main/java/at/ac/fhstp/crs/menu/IMenu.java
@@ -1,7 +1,12 @@
 package at.ac.fhstp.crs.menu;
 
+import at.ac.fhstp.crs.Token;
+
+import java.util.List;
+
 public interface IMenu {
     void displayInitMenu();
-    void displayPopularTokens();
+    void displayPopularTokens(List<Token> tokenList);
+
     void displayTopMoverTokens();
 }
