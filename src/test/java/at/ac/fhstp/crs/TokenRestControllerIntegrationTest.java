c21a286c928beb194b7662988293a6fd383b9cd4
Samuel Ritter <is201037@fhstp.ac.at>
2022-05-17 15:09:22 +0200

    Fixed Bean Problems

--- src/test/java/at/ac/fhstp/crs/TokenRestControllerIntegrationTest.java
+++ src/test/java/at/ac/fhstp/crs/TokenRestControllerIntegrationTest.java
@@ -4,23 +4,17 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import at.ac.fhstp.crs.controller.TokenController;
 import at.ac.fhstp.crs.model.Token;
-import at.ac.fhstp.crs.repository.TokenRepository;
-import at.ac.fhstp.crs.service.AService;
 import java.util.Arrays;
 import java.util.List;
 
-import org.aspectj.lang.annotation.Before;
+import at.ac.fhstp.crs.service.TokenService;
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -32,11 +26,12 @@ import org.springframework.web.context.WebApplicationContext;
 @SpringBootTest
 public class TokenRestControllerIntegrationTest {
 
-  @Mock
-  AService<Token> tokenService;
+  @MockBean
+  TokenService tokenService;
 
   @Autowired
   WebApplicationContext webApplicationContext;
+
   MockMvc mockMvc;
 
   @Test
@@ -47,10 +42,10 @@ public class TokenRestControllerIntegrationTest {
     Mockito.when(tokenService.getAll()).thenReturn(tokens);
 
     mockMvc
-      .perform(get("/token"))
-      .andExpect(status().isOk())
-      .andExpect(jsonPath("$", Matchers.hasSize(1)))
-      .andExpect(jsonPath("$[0].symbol", Matchers.is("BTC")));
+            .perform(get("/token"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$", Matchers.hasSize(1)))
+            .andExpect(jsonPath("$[0].symbol", Matchers.is("BTC")));
   }
 
   @BeforeEach
